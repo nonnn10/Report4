872a5257bb06e3f7dd1137b4c86ea98257092d77
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-11-24 08:59:15 +0900

    2回目

--- src/Report4.java
+++ src/Report4.java
@@ -1,6 +1,12 @@
 public class Report4 {
-    public static void main (String[] args){
-        String str = null;
-        System.out.println(str.length());
+    public static void main(String[] args) {
+        try {
+            String str = null;
+            System.out.println(str.length());
+        } catch (Exception e) {
+            System.out.println("NullPointerExceptionが発生しました。");
+            System.out.println(e.getMessage());
+        }
+
     }
 }
