db9e0575fdaacd6722be9729ceb22baaafbaa88e
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-11-24 12:54:33 +0900

    3回目

--- src/Report4.java
+++ src/Report4.java
@@ -1,12 +1,6 @@
 public class Report4 {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (Exception e) {
-            System.out.println("NullPointerExceptionが発生しました。");
-            System.out.println(e.getMessage());
-        }
-
+            String str = "3．14";
+            Double.parseDouble(str);
     }
 }
