33087ffbafdcc000e6d1fbec6f375a90b9fd3fd9
Shinnosuke Ooshiro <e175755@ie.u-ryukyu.ac.jp>
2017-11-27 20:32:15 +0900

    3回目

--- src/Report4.java
+++ src/Report4.java
@@ -1,6 +1,6 @@
 public class Report4 {
     public static void main(String[] args) {
             String str = "3．14";
-            Double.parseDouble(str);
+            double value = Double.parseDouble(str);
     }
 }
